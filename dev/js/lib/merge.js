63ff93479e74f4436ac2d1fcfcb39acc66eed387
hans-sperling <info@bluejade.de>
2016-08-17 15:52:43 +0200

    Lib files added.

---
+++ dev/js/lib/merge.js
@@ -0,0 +1,46 @@
+function merge(target, src) {
+    'use strict';
+
+    var array = Array.isArray(src),
+        dst   = array && [] || {};
+
+    if (array) {
+        target = target || [];
+        dst = dst.concat(target);
+        src.forEach(function (e, i) {
+            if (typeof dst[i] === 'undefined') {
+                dst[i] = e;
+            }
+            else if (typeof e === 'object') {
+                dst[i] = deep(target[i], e);
+            }
+            else {
+                if (target.indexOf(e) === -1) {
+                    dst.push(e);
+                }
+            }
+        });
+    }
+    else {
+        if (target && typeof target === 'object') {
+            Object.keys(target).forEach(function (key) {
+                dst[key] = target[key];
+            })
+        }
+        Object.keys(src).forEach(function (key) {
+            if (typeof src[key] !== 'object' || !src[key]) {
+                dst[key] = src[key];
+            }
+            else {
+                if (!target[key]) {
+                    dst[key] = src[key];
+                }
+                else {
+                    dst[key] = deep(target[key], src[key]);
+                }
+            }
+        });
+    }
+
+    return dst;
+}
